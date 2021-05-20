b6915e510ee365f44bf03a8aa370ac851d4e299a
tecshuttle <tecshuttle@gmail.com>
2021-05-20 21:27:02 +0800

    学习了第一道算法题。

--- 2 前端/readme.md
+++ 2 前端/readme.md
@@ -445,6 +445,28 @@ Semi
 
 - Version Lens 显示 package.json 文件中每个软件包的最新版本。
 
+## jshint
+
+```json
+{
+  "esversion": 6, //指定的ECMAScript版本代码
+  "sub": true, //允许 [] . 形式来访问对象
+  "laxbreak": true, //不检查换行
+  "regexp": true, //不允许使用.和[^...]的正则
+  "asi": true, //无视没有加分号的行尾
+  "bitwise": true, //会禁用位运算符
+  "curly": true, //要求使用if和while等结构语句时加上{}来明确代码块
+  "immed": true, //要求匿名函数的调用为(function(){//}());
+  "eqeqeq": true, //要求代码中使用===或!==，而不是使用==和!=
+  "eqnull": true, //允许使用"== null"作比较
+  "newcap": true, //要求每一个构造函数名都要大写字母开头
+  "noarg": true, //禁止arguments.caller和arguments.callee的使用
+  "undef": true, //要求所有的非全局变量，在使用前都被声明
+  "strict": false, //要求使用use strict;语法
+  "devel": true
+}
+```
+
 ## 其它
 
 [从VSCode看大型IDE技术架构](https://zhuanlan.zhihu.com/p/96041706)
--- 4 算法/leetcode.js
+++ 4 算法/leetcode.js
@@ -2,30 +2,77 @@
 // 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
 // 你可以按任意顺序返回答案。
 
-const nums = [2, 7, 11, 15];
-
-const target = 6;
-
+// 暴力枚举：6264ms 击败5.88%的人
 const add = (nums, target) => {
-  let a = 0,
-    b = 0;
-
   for (const i in nums) {
-    console.log(i);
-    a = nums[i];
-    let numsB = [...nums];
-    numsB.splice(i, 1);
-    console.log(numsB);
-
-    for (const j in numsB) {
-      b = numsB[j];
-      console.log(a + b);
-      if (a + b === target) {
-        console.log(`${a} + ${b} = ${target}`);
-        return [i * 1, j * 1 + 1];
+    for (const j in nums) {
+      if (nums[i] + nums[j] === target && i !== j) {
+        return [i * 1, j * 1];
       }
     }
   }
 };
 
-console.log(add(nums, 9));
+// 动态哈希表法：96ms 击败8.16%的人
+const add2 = (nums, target) => {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const num1 = nums[i];
+    const num2 = target - nums[i];
+    if (map.has(num2)) {
+      return [map.get(num2), i];
+    } else {
+      map.set(num1, i);
+    }
+  }
+};
+
+// 静态哈希表法：88ms 击败23.83%的人
+const add3 = (nums, target) => {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    map.set(nums[i], i);
+  }
+  for (let i = 0; i < nums.length; i++) {
+    const diff = target - nums[i];
+    if (map.has(diff) && map.get(diff) !== i) {
+      return [i, map.get(diff)];
+    }
+  }
+};
+
+// 哈希表法：92ms 击败33.76%的人
+const add4 = (nums, target) => {
+  let map = new Map();
+  for (let i = 0, len = nums.length; i < len; i++) {
+    if (map.has(target - nums[i])) {
+      return [map.get(target - nums[i]), i];
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+  return [];
+};
+
+// 72ms  击败97.2%的人
+var twoSum = function (nums, target) {
+  const prevNums = {}; // 存储出现过的数字，和对应的索引
+
+  for (let i = 0; i < nums.length; i++) {
+    // 遍历元素
+    const curNum = nums[i]; // 当前元素
+    const targetNum = target - curNum; // 满足要求的目标元素
+    const targetNumIndex = prevNums[targetNum]; // 在prevNums中获取目标元素的索引
+    if (targetNumIndex !== undefined) {
+      // 如果存在，直接返回 [目标元素的索引,当前索引]
+      return [targetNumIndex, i];
+    } else {
+      // 如果不存在，说明之前没出现过目标元素
+      prevNums[curNum] = i; // 存入当前的元素和对应的索引
+    }
+  }
+};
+
+console.log(add([2, 3, 4], 6));
+console.log(add([2, 7, 11, 15], 13));
+console.log(add([2, 7, 11, 15], 26));
